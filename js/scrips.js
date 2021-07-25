73de1964a07c4f685a54475c657958dd82d1239c
Recflex <87237591+Recflex@users.noreply.github.com>
2021-07-25 02:03:29 -0300

    Add files via upload

---
+++ js/scrips.js
@@ -0,0 +1,12 @@
+var cantidad1 = parseFloat (prompt ('Ingrese el primer numero'));
+var cantidad2 = parseFloat (prompt ('Ingrese el segundo numero'));
+
+var suma = cantidad1 + cantidad2;
+var resta = cantidad1 - cantidad2;
+var multi = cantidad1 * cantidad2;
+var div = cantidad1 / cantidad2;
+
+document.write('<p>suma:' +suma+ '</p>');
+document.write('<p>resta:' +resta+ '</p>');
+document.write('<p>multiplicasión:' +multi+ '</p>');
+document.write('<p>División:' +div+ '</p>');
